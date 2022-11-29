13ab7c334260a8a2d19295b7b043e8493ed410e1
ha520568@gmail.com <ha520568@gmail.com>
2022-11-29 16:01:52 +0500

    Conversion of Assembly to hex file

--- make.bat
+++ make.bat
@@ -1,3 +1,4 @@
+//Conversion of C to hex file
 rmdir -r build
 mkdir build
 riscv64-unknown-elf-as -c -o build/startup.o src/startup.s -march=rv32i -mabi=ilp32
@@ -7,3 +8,13 @@ riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text
 python3 maketxt.py build/main.bin > build/main.txt
 riscv64-unknown-elf-objdump -S -s build/main.elf > build/main.dump
 
+//Conversion of Assembly to hex file
+
+::riscv64-unknown-elf-as -c -o build/startup.o src/startup.s -march=rv32i -mabi=ilp32
+rmdir -r build
+mkdir build
+riscv64-unknown-elf-as -c -o build/asm_to_m_code_code.o src/asm_to_m_code.s -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-gcc -o build/asm_to_m_code.elf build/asm_to_m_code.o -T linker.ld -nostdlib -march=rv32i -mabi=ilp32
+riscv64-unknown-elf-objcopy -O binary --only-section=.data* --only-section=.text* build/asm_to_m_code.elf build/asm_to_m_code.bin
+python3 maketxt.py build/asm_to_m_code.bin > build/asm_to_m_code.txt
+riscv64-unknown-elf-objdump -S -s build/asm_to_m_code.elf > build/asm_to_m_code.dump
